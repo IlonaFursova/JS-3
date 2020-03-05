423edb1b18f39354dec7ff853a830e6c64aa3874
IlonaFursova <61581609+IlonaFursova@users.noreply.github.com>
2020-03-05 23:16:09 +0200

    Add files via upload

---
+++ task1-4.js
@@ -0,0 +1,18 @@
+//Task 1
+// let min1 = Math.min.apply(null, [1, 2, 3, 4, 5]);
+// let min2 = Math.min.apply(null, [5, 6, 7, 8, 9]);
+// let min3 = Math.min.apply(null, [20, 21, 34, 56, 100]);
+
+// alert(min1 + min2 + min3);
+
+//Task 2
+// let arr = [1, 2, 10, 8];
+// arr.sort((a, b) => a - b);
+// //сортировка по возрастанию
+// let arr2 = arr[arr.length - 1];
+// let arr3 = arr[arr.length - 2];
+// alert(`${arr2}, ${arr3}`);
+
+// //Task 3
+
+
